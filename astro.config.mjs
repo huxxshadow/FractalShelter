c7f9133d7634ed4c90a9f1b5c1697cc2cdad0bf7
ZhenYu Hu <huxxshadowhzy@gmail.com>
2026-02-21 23:36:13 +0800

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 export default defineConfig({
     site: 'https://huxxshadow.github.io',
     base: '/FractalShelter',
-
+    trailingSlash: "always",
     vite: {
         plugins: [tailwindcss()],
     },
